cf02b08e427aa1b9c3fb8fffaf2f49a8ace2e442
Quinten Coret <Qcoret@gmail.com>
2024-03-14 15:08:47 +0100

    answers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,17 @@
 
   let { data } = $props()
 
+  let foundWords = $state<string[]>([])
   let word = $state('')
+
+  const score = $derived(
+    foundWords.reduce((score, word) => {
+      const pangram = data.letters.every((l) => word.includes(l))
+      if (pangram) return score + word.length + 7
+      if (word.length === 4) return score + 1
+      return score + word.length
+    }, 0)
+  )
 
   let otherLetters = $state(data.letters.filter((l) => l !== data.letter))
   $effect(() => void (otherLetters = data.letters.filter((l) => l !== data.letter)))
@@ -25,16 +35,22 @@
   }
 
   function onKeydown(event: KeyboardEvent) {
-    if (event.key === 'Backspace') {
-      word = word.slice(0, -1)
-    }
-    if (event.key.match(/^[a-z]$/i)) {
-      word += event.key
-    }
+    if (event.key === 'Enter') onSubmit()
+    if (event.key === 'Backspace') word = word.slice(0, -1)
+    if (event.key.match(/^[a-z]$/i)) word += event.key
   }
 
   function onPress(letter: string) {
     word += letter
+  }
+
+  function onSubmit() {
+    if (word.length < 4) return
+    if (foundWords.includes(word)) return
+    if (!word.includes(data.letter)) return
+    if (!data.answers.includes(word)) return
+    foundWords.push(word)
+    word = ''
   }
 </script>
 
@@ -45,7 +61,16 @@
   <h1 class="text-2xl font-bold">Spellingsbij</h1>
 </header>
 
-<div class="mx-6 my-4 rounded-md border border-gray-200 px-4 py-2 text-gray-500">Woorden</div>
+<b class="mx-6 my-4 block">{score}</b>
+
+<div class="mx-6 my-4 flex gap-2 rounded-md border border-gray-200 px-4 py-2">
+  {#each foundWords as word}
+    <span class="capitalize">{word}</span>
+  {/each}
+  {#if foundWords.length < 1}
+    <span class="text-gray-500">Geen woorden gevonden</span>
+  {/if}
+</div>
 
 <div class="mx-auto my-4 flex h-10 w-max items-center">
   <div class="mr-1">
@@ -67,7 +92,7 @@
 <div class="mx-auto my-8 flex w-fit gap-2">
   <button onclick={onDelete} class="rounded-full border px-4 py-2">Wis</button>
   <button onclick={onShuffle} class="rounded-full border px-4 py-2">&#x1f500;</button>
-  <button class="rounded-full border px-4 py-2">Oké</button>
+  <button onclick={onSubmit} class="rounded-full border px-4 py-2">Oké</button>
 </div>
 
 <style>
